5fd6e30dabe2a4405cc0dc5530cb42dc9b5468bb
Laturkar <SL8724@zebra.com>
2024-03-31 22:39:12 +0530

    Updated the HTML and TS files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';  @Component({   selector: 'app-root',   templateUrl: './app.component.html',   styleUrls: ['./app.component.css'] }) export class AppComponent {   title = 'My First Angular App'; }
 import { RouterOutlet } from '@angular/router';
 
 @Component({
